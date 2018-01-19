521f5ac84f353103568c202b9ee2025a6f7dfe5b
mzlovelyj <liljp064@gmail.com>
2018-01-18 21:38:41 -0500

    one more quiz

--- Javascript/QuizCheckingYourBalance3-5/checkAndBalance.js
+++ Javascript/QuizCheckingYourBalance3-5/checkAndBalance.js
@@ -4,15 +4,13 @@
 
 var balance = 1200.90;
 var checkBalance = true;
-//changing isActive to tue displays the balance!
+//changing isActive to true displays the balance!
 var isActive = true;
 
 
 // your code goes here
 if (checkBalance && isActive && balance > 0) {
     console.log('Your balance is $' + balance.toFixed(2) + '.');
-} else if (!checkBalance) {
-    console.log('Thank you, have a nice day!');
 } else if (!isActive) {
     console.log('Your account is no longer active');
 } else if (balance === 0) {
@@ -20,5 +18,5 @@ if (checkBalance && isActive && balance > 0) {
 } else if (balance < 0) {
     console.log('Your balance is Negative. Please contact your bank.');
 } else {
-    console.log('Have a great day, please come again.');
+    console.log('Thank you, have a nice day!');
 }

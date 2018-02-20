dd2df5137f8291fc59dba40562e2ca68a45155e9
mzlovelyj <liljp064@gmail.com>
2018-02-19 19:58:12 -0500

    Fix Another Error in for loop Test

--- Javascript/Quiz Fix the Error 1.4-5/fixTheError.js
+++ Javascript/Quiz Fix the Error 1.4-5/fixTheError.js
@@ -1,18 +1,17 @@
-/*
-  Programming Quiz: Fix the Error 1 (4-5)
+/* Programming Quiz: Fix the Error 1 (4-5)
  Directions:
 Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
 
 for (x < 10; x++) {
   console.log(x);
-}*/
+} */
 
 // fix the for loop
 for (var x = 5; x < 10; x++) {
     console.log(x);
 }
 
-/*Remember that a for needs the following: 
+/* Remember that a for needs the following: 
 for ( start; stop; step ) {
   // do this thing
-}*/
+} */
---
+++ Javascript/Quiz.Fix the Error 2. 4-6/fixTheError.2.js
@@ -0,0 +1,15 @@
+/* Programming Quiz: Fix the Error 2 (4-6)
+
+// fix the for loop
+for (var k = 0 k < 200 k++) {
+    console.log(k);
+} */
+
+for (var k = 0; k < 200; k++) {
+    console.log(k);
+}
+
+/*Remember that a for needs the following: 
+for ( start; stop; step ) {
+  // do this thing
+}*/

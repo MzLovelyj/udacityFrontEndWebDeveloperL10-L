c463f36911e9ef5cf58cac93402126beac640ffe
mzlovelyj <liljp064@gmail.com>
2018-03-03 15:55:34 -0500

    completed 1 more exercise.

---
+++ Javascript/Quiz: Laugh 5-4/laugh.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(l) {
+        var ha = "";
+        for (x = 0; x < l; x++) {
+            ha += "ha";
+        }
+
+        return ha + "!";
+    }
+    /* finish the function expression */
+
+console.log(laugh(10));
